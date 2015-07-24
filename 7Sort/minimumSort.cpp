78a6397e16020e8d05701039a3dc711ec62ff784
Kazushi Kawamura <wakaka2277@gmail.com>
2015-07-24 16:50:23 +0900

    implemented minumum cost

---
+++ 7Sort/minimumSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+const int MAX = 1001;
+
+int main() {
+    int n, w[MAX];
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &w[i]);
+    }
+
+
+    return 0;
+}
+
